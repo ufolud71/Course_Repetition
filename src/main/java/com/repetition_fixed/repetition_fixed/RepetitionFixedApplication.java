b1f79eae69c2d728863ca740487d2d4f16770e22
ufolud71 <chudy.orfeusz@gmail.com>
2023-03-07 16:50:58 +0100

    Commit

---
+++ src/main/java/com/repetition_fixed/repetition_fixed/RepetitionFixedApplication.java
@@ -0,0 +1,14 @@
+package com.repetition_fixed.repetition_fixed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetitionFixedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepetitionFixedApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/repetition_fixed/repetition_fixed/RepetitionFixedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repetition_fixed.repetition_fixed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepetitionFixedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
